a2f0b1963d76557473c824f94907d2ebb16df51c
Urantij <maximumuranti@gmail.com>
2024-11-03 07:54:21 +0700

    я нажал формат документ

--- Analyze/MapDownloader.cs
+++ Analyze/MapDownloader.cs
@@ -5,15 +5,15 @@ public class MapDownloader : IDisposable
     private readonly HttpClient _client;
 
     private readonly Uri _baseUrl = new("https://catboy.best/osu/");
-    
+
     public MapDownloader()
     {
         _client = new HttpClient();
         _client.Timeout = TimeSpan.FromSeconds(20);
-        
+
         _client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Osu News");
     }
-    
+
     public async Task<Models.MapData> DownloadAsync(ulong id, CancellationToken cancellationToken = default)
     {
         Uri uri = new(_baseUrl, id.ToString());
